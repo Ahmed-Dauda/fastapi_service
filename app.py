07340160c2e4500a5ca264db01f9c9b88e266bc7
Ahmed-Dauda <addatainsight@gmail.com>
2025-09-18 08:46:22 +0100

    first commit

--- app.py
+++ app.py
@@ -1,34 +1,30 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine, MetaData, Table, select
-
-app = FastAPI()
-
-
-
-@app.get("/test")
-async def root():
-    return {"message": "Hello World 222"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+from sqlalchemy import MetaData, Table, select
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # Testing
-# DATABASE_URL = "postgresql+psycopg2://fastapiuser:fastapi37811@localhost:5432/fastapidb"
+# DATABASE_URL = "postgresql+asyncpg://fastapiuser:fastapi37811@localhost:5432/fastapidb"
 # Production
-DATABASE_URL = "postgresql+psycopg2://ufo30bm52onkok:pd17fb510bee0d9e5ee2defb9a9d002afeaeead55b9e9a80478940f8b8d039737@c6oob9dspeco5.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d9lpb17qpeanjp"
+# DATABASE_URL = "postgresql+psycopg2://ufo30bm52onkok:pd17fb510bee0d9e5ee2defb9a9d002afeaeead55b9e9a80478940f8b8d039737@c6oob9dspeco5.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d9lpb17qpeanjp"
+DATABASE_URL = "postgresql+asyncpg://ufo30bm52onkok:pd17fb510bee0d9e5ee2defb9a9d002afeaeead55b9e9a80478940f8b8d039737@c6oob9dspeco5.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d9lpb17qpeanjp"
 
-engine = create_engine(DATABASE_URL)
+
+
+engine = create_async_engine(DATABASE_URL, echo=True)
 metadata = MetaData()
+app = FastAPI()
 
 @app.get("/api/program-pages")
 async def get_program_pages():
-    metadata.reflect(bind=engine)
-    program_pages_table = Table("django_fastapi_programpage", metadata, autoload_with=engine)
-    with engine.connect() as conn:
-        result = conn.execute(select(program_pages_table))
+    async with engine.connect() as conn:
+        # Reflect inside run_sync
+        await conn.run_sync(metadata.reflect)
+        program_pages_table = Table("django_fastapi_programpage", metadata, autoload_with=conn)
+        result = await conn.execute(select(program_pages_table))
+        rows = result.fetchall()
+
         pages = [
             {
                 "id": row.id,
@@ -36,6 +32,31 @@ async def get_program_pages():
                 "description": row.description,
                 "benefits": row.benefits,
             }
-            for row in result.fetchall()
+            for row in rows
         ]
     return pages
+
+
+@app.get("/test")
+async def root():
+    return {"message": "Hello World. learners"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+origins = [
+    "http://localhost:3000",             # React local dev
+    "https://codethinkers.netlify.app",  # Your deployed React site
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # restrict to these domains
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
